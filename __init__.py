86637286a1aa4e17a15aeeb776b3a0eb62cf62fb
John Bartkiw <14988042+johnbartkiw@users.noreply.github.com>
2019-03-02 14:34:20 -0800

    - dumb

--- __init__.py
+++ __init__.py
@@ -77,7 +77,7 @@ class TuneinSkill(MycroftSkill):
                         wait_while_speaking()
                         LOG.debug("Found stream URL: " + self.url)
                         #self.audio_service.play(self.url)
-                        self.process = play_mp3(random.choice(urls))
+                        self.process = play_mp3(self.url)
                         return
 
         # We didn't find any playable stations
