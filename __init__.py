53f2245a54e157b2dab836f379ea4bbb3ddf1472
John Bartkiw <14988042+johnbartkiw@users.noreply.github.com>
2019-03-01 23:27:20 -0800

    - First real drop of code. - Sets license properly - Gets rid of templay dialog and vocab files - Sets up initial dialog and vocab files - Initial logic code to serach for the best URL from Tunein API - Currently intent uses a hardcoded search query for testing purposes

--- __init__.py
+++ __init__.py
@@ -1,65 +1,85 @@
-# TODO: Add an appropriate license to your skill before publishing.  See
-# the LICENSE file for more information.
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 John Bartkiw
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-# Below is the list of outside modules you'll be using in your skill.
-# They might be built-in to Python, from mycroft-core or from external
-# libraries.  If you use an external library, be sure to include it
-# in the requirements.txt file so the library is installed properly
-# when the skill gets installed later by a user.
+import requests
+from xml.dom.minidom import parseString
 
 from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.skills.core import intent_handler
 from mycroft.util.log import LOG
 
-# Each skill is contained within its own class, which inherits base methods
-# from the MycroftSkill class.  You extend this class as shown below.
+from mycroft.audio import wait_while_speaking
+from mycroft.messagebus.message import Message
+from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
 
-# TODO: Change "Template" to a unique name for your skill
-class TemplateSkill(MycroftSkill):
+# Static values for tunein search requests
+base_url = "http://opml.radiotime.com/Search.ashx"
+headers = {}
 
-    # The constructor of the skill, which calls MycroftSkill's constructor
+class TuneinSkill(CommonPlaySkill):
+
     def __init__(self):
-        super(TemplateSkill, self).__init__(name="TemplateSkill")
-        
-        # Initialize working variables used within the skill.
-        self.count = 0
+        super().__init__(name="TuneinSkill")
 
-    # The "handle_xxxx_intent" function is triggered by Mycroft when the
-    # skill's intent is matched.  The intent is defined by the IntentBuilder()
-    # pieces, and is triggered when the user's utterance matches the pattern
-    # defined by the keywords.  In this case, the match occurs when one word
-    # is found from each of the files:
-    #    vocab/en-us/Hello.voc
-    #    vocab/en-us/World.voc
-    # In this example that means it would match on utterances like:
-    #   'Hello world'
-    #   'Howdy you great big world'
-    #   'Greetings planet earth'
-    @intent_handler(IntentBuilder("").require("Hello").require("World"))
-    def handle_hello_world_intent(self, message):
+        self.audio_state = "stopped"  # 'playing', 'paused', 'stopped'
+        self.station_name = None
+        self.url = None
+
+    @intent_handler(IntentBuilder("").require("Station").require("TuneIn"))
+    def handle_station_search_intent(self, message):
         # In this case, respond by simply speaking a canned response.
         # Mycroft will randomly speak one of the lines from the file
         #    dialogs/en-us/hello.world.dialog
-        self.speak_dialog("hello.world")
+        self.find_station("Jazz 24")
 
-    @intent_handler(IntentBuilder("").require("Count").require("Dir"))
-    def handle_count_intent(self, message):
-        if message.data["Dir"] == "up":
-            self.count += 1
-        else:  # assume "down"
-            self.count -= 1
-        self.speak_dialog("count.is.now", data={"count": self.count})
+    def stop(self):
+        self.audio_state = "stopped"
+        self.station_name = None
+        self.url = None
+        return True
 
-    # The "stop" method defines what Mycroft does when told to stop during
-    # the skill's execution. In this case, since the skill's functionality
-    # is extremely simple, there is no need to override it.  If you DO
-    # need to implement stop, you should return True to indicate you handled
-    # it.
-    #
-    # def stop(self):
-    #    return False
+    # Attempt to find the first active station matching the query string
+    def find_station(search_term):
+        payload = { "query" : search_term }
+        # get the response from the TuneIn API
+        res = requests.post(url, data=payload, headers=headers)
+        # results are each in their own <outline> tag as defined by OPML (https://en.wikipedia.org/wiki/OPML)
+        entries = dom.getElementsByTagName("outline")
 
-# The "create_skill()" method is used to create an instance of the skill.
-# Note that it's outside the class itself.
+        # Loop through outlines in the lists
+        for entry in entries:
+            # Only look at outlines that are of type=audio and item=station
+            if (entry.getAttribute("type") == "audio") and (entry.getAttribute("item") == "station"):
+                    if (entry.getAttribute("key") == "unavailable"):
+                        # Ignore entries that are marked as unavailable
+                    else:
+                        self.url = entry.getAttribute("URL")
+                        self.station_name = entry.getAttribute("text")
+                        self.audio_state = "playing"
+                        self.speak_dialog("now.playing", {"station": self.station_name} )
+                        break
+
+        # We didn't find any playable stations
+        self.speak_dialog("not.found")
+
 def create_skill():
-    return TemplateSkill()
+    return TuneinSkill()
