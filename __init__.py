6b4cf98555bd51833a5d1973cf0b935889b1d74f
John Bartkiw <14988042+johnbartkiw@users.noreply.github.com>
2019-03-02 17:19:50 -0800

    - Another try at audioservice

--- __init__.py
+++ __init__.py
@@ -82,8 +82,10 @@ class TuneinSkill(MycroftSkill):
                         self.speak_dialog("now.playing", {"station": self.station_name} )
                         wait_while_speaking()
                         LOG.debug("Found stream URL: " + self.stream_url)
-                        #self.audio_service.play(self.stream_url)
-                        self.process = play_mp3(self.stream_url)
+                        if (self.audio_service):
+                            self.audio_service.play(self.stream_url)
+                        else:
+                            self.process = play_mp3(self.stream_url)
                         return
 
         # We didn't find any playable stations
@@ -99,7 +101,9 @@ class TuneinSkill(MycroftSkill):
 
     def stop(self):
         if self.audio_state == "playing":
-            #self.audio_service.stop()
+            if (self.audio_service):
+                self.audio_service.stop()
+                
             if self.process and self.process.poll() is None:
                self.process.terminate()
                self.process.wait()
